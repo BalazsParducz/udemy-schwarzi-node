74d01438af2a23b7262f6d7fba60c68969e30b6d
BalazsParducz <parducz.balazs@gmail.com>
2019-05-27 17:02:32 +0200

    71

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
-console.log(routes.someText);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-const server = http.createServer(routes.handler);
+const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
